9c4a25f26be841eaa0a94fe35d8a4315f2945305
byte-engineer <bilalalasmayly@gmail.com>
2026-07-15 10:48:45 +0400

    Some layout adjestments

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,13 @@
 use csv::{Reader, Writer};
-use eframe::egui;
+use eframe::egui::{self, Vec2b};
 use egui_plot::{Line, Plot, PlotPoints, Points};
 use rand::Rng;
 use rustfft::{FftPlanner, num_complex::Complex};
-use std::f64::consts::PI;
+use std::{f64::consts::PI, process::exit};
+use puffin::{profile_function, profile_scope};
 
-const RECORD_LENGTH: usize = 100;
+
+const RECORD_LENGTH: usize = 10000;
 
 pub struct RustyApp {
     frame_count: u64,
@@ -47,15 +49,16 @@ impl RustyApp {
             fft_points: vec![Complex { re: 0.0, im: 0.0 }; RECORD_LENGTH],
             enable_fft: true,
             enable_points: true,
-            sine_freq: 0.00010,
-            auto_bounds: true,
+            sine_freq: 10.0,
+            auto_bounds: false,
             waveform: WaveForm { 
-                points: Vec::new(), sampling_rate: 100, record_length: RECORD_LENGTH
+                points: Vec::new(), sampling_rate: 1000, record_length: RECORD_LENGTH
             }
         }
     }
 
     fn update_points(&mut self) {
+        profile_function!("Update Points");
         let mut rng = rand::rng();
 
         self.waveform.points.resize(self.waveform.record_length, Point::default());
@@ -64,52 +67,70 @@ impl RustyApp {
         for (i, p) in self.waveform.points.iter_mut().enumerate() {
             p.x = i as f64;
             p.y = rng.random_range(-0.01f64..=0.01f64)
-                + (((i as f64) * 2.0 * PI * self.sine_freq/self.waveform.sampling_rate as f64).sin()).signum() * 10.0
-                + ((i as f64) * 10.0 * PI * self.sine_freq/self.waveform.sampling_rate as f64).sin()
+                + ((i as f64) * 2.0 * PI * self.sine_freq/self.waveform.sampling_rate as f64).sin();
         }
     }
 }
 
+
+
+
 impl eframe::App for RustyApp {
     fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
 
+        puffin::GlobalProfiler::lock().new_frame();
         self.frame_count += 1;
 
         // Update simulation
         self.update_points();
+        let fps = (1.0 / ui.input(|i| i.stable_dt)).round();
 
-        egui::CentralPanel::default().show(ui, |ui| {
-            if ui.button("Save CSV").clicked() {
-                save_points("data.csv", &self.waveform.points);
+        
+        ui.input(|i| {
+            if i.key_pressed(egui::Key::Escape) {
+                // ui.ctx().send_viewport_cmd(egui::ViewportCommand::Close);
+                exit(0);
             }
+        });
+
+        
+        egui::CentralPanel::default().show(ui, |ui| {
+            
+            // let dropped_files = ui.ctx().input(|i| i.raw.dropped_files.clone());
+    
+    
+            // for file in dropped_files {
+            //     if let Some(path) = file.path {
+            //         println!("Dropped: {}", path.display());
+    
+            //         // Load your file here
+            //         // self.load_file(&path);
+            //     }
+            // }
+    
+            // let hovering = ui.ctx().input(|i| !i.raw.hovered_files.is_empty());
+    
+            // if hovering {
+            //     egui::Area::new("drop_overlay".into())
+            //         .fixed_pos(egui::pos2(20.0, 20.0))
+            //         .show(ui.ctx(), |ui| {
+            //             ui.label("Drop files here");
+            //         });
+            // }
+
+
 
             let mut planner = FftPlanner::new();
             let fft = planner.plan_fft_forward(self.fft_points.len());
             self.fft_points = self.waveform.points
-                .iter()
+            .iter()
                 .map(|x| Complex { re: x.y, im: 0.0 })
                 .collect();
 
-            fft.process(&mut self.fft_points);
-
-            ui.label(format!("Frame: {}", self.frame_count));
-
-            ui.add(
-                egui::Slider::new(&mut self.sine_freq, 0.001..=1000.0)
-                    .logarithmic(true)
-                    .text("Frequency"),
-            );
-
-            let response = ui.add(
-                egui::Slider::new(&mut self.waveform.record_length, 10..=1000000)
-                    .logarithmic(true)
-                    .text("Record length")
-            );
-
-            ui.add(
-                egui::Slider::new(&mut self.waveform.sampling_rate, 2..=1000)
-                    .text("Sampling Rate"),
-            );
+            {
+                puffin::profile_scope!("Process FFT");
+                fft.process(&mut self.fft_points);
+            }
 
             let plot_points_for_scatter =
                 PlotPoints::from_iter(self.waveform.points.iter().map(|p| [p.x, p.y]));
@@ -134,16 +155,76 @@ impl eframe::App for RustyApp {
 
             let fft_line = Line::new("FFT", plot_points_for_fft);
 
-            ui.checkbox(&mut self.line_chked, "Draw Lines.");
-            ui.checkbox(&mut self.enable_fft, "Draw FFT.");
-            ui.checkbox(&mut self.enable_points, "Draw points.");
-            ui.checkbox(&mut self.auto_bounds, "auto bounds.");
+            ui.horizontal(|ui| {
 
-            Plot::new("Combined Plot")
-                .show_x(true)
-                .show_y(true)
+                profile_scope!("Building UI");
+                main_frame(ui, |ui| {
+                    ui.vertical(|ui| {
+                        ui.label(format!("Frame: {}", self.frame_count));
+                        ui.label(format!("FPS  : {}", fps));
+                        // if ui.button("Save CSV").clicked() {
+                        //     save_points("data.csv", &self.waveform.points);
+                        // }
+
+                        ui.checkbox(&mut self.auto_bounds, "Auto Bounds");
+                    });
+                });
+
+                main_frame(ui, |ui| {
+                    ui.vertical(|ui| {
+                        ui.add(
+                            egui::Slider::new(&mut self.sine_freq, 0.1..=10000.0)
+                                .logarithmic(false)
+                                .text("Frequency"),
+                        );
+
+                        ui.add(
+                            egui::Slider::new(&mut self.waveform.sampling_rate, 2..=10000)
+                                .text("Sampling Rate"),
+                        );
+            
+                        ui.add(
+                            egui::Slider::new(&mut self.waveform.record_length, 10..=80000)
+                                .logarithmic(true)
+                                .text("Record length")
+                        );
+            
+                    });
+                });
+    
+    
+                main_frame(ui, |ui|{  
+                    ui.vertical(|ui| {
+                        ui.checkbox(&mut self.enable_fft, "Draw FFT.");
+                        ui.checkbox(&mut self.enable_points, "Draw points.");
+                        if self.enable_points {
+                            ui.indent("points settings", |ui| {
+                                ui.checkbox(&mut self.line_chked, "Draw Lines.");
+                            });
+                        }
+                    })
+                });
+            });
+
+
+            Plot::new("Points & fft Plot")
+                .x_axis_formatter(|mark, _range| {
+                    match mark.value {
+                        x if x == 0.0 => "DC".to_owned(),
+                        x => format!(
+                            "{:.1} Hz\n{:.1} sec",
+                            x*(self.waveform.sampling_rate as f64)/(self.waveform.record_length as f64),
+                            x/(self.waveform.sampling_rate as f64)),
+                    }
+                })
+                .y_axis_formatter(|mark, _range| {
+                    match mark.value {
+                        x => format!("{} db", (x as f64))
+                    }
+                })
+
                 .show(ui, |plot_ui| {
-
+                    puffin::profile_scope!("Plotting");
                     if self.enable_points {
                         if self.line_chked {
                             plot_ui.line(line);
@@ -152,11 +233,16 @@ impl eframe::App for RustyApp {
                         }
                     }
 
+                    if self.auto_bounds {
+                        plot_ui.set_auto_bounds(Vec2b::new(true, true));
+                    }
+
                     if self.enable_fft {
                         plot_ui.line(fft_line);
                     }
-                });
-        });
+
+                }); // plot .show()
+        }); // Central panel
 
         ui.request_repaint();
     }
@@ -178,6 +264,7 @@ fn fetch_points(path: &str) -> Vec<Point> {
         .collect()
 }
 
+#[allow(unused)]
 fn save_points(path: &str, points: &[Point]) {
     let mut writer = Writer::from_path(path).unwrap();
 
@@ -188,4 +275,20 @@ fn save_points(path: &str, points: &[Point]) {
     }
 
     writer.flush().unwrap();
+}
+
+
+
+fn main_frame<R>(
+    ui: &mut egui::Ui,
+    add_contents: impl FnOnce(&mut egui::Ui) -> R,
+) -> egui::InnerResponse<R> {
+    egui::Frame::new()
+        .stroke(egui::Stroke::new(1.0, egui::Color32::DARK_GRAY))
+        .inner_margin(5.0)
+        .outer_margin(1.0)
+        .show(ui, |ui|{
+            ui.set_min_height(85.0);
+            add_contents(ui)
+        })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,42 @@
 mod app;
 
+
 fn main() {
+
+    start_puffin_server(); // NOTE: you may only want to call this if the users specifies some flag or clicks a button!
+
+
     let options = eframe::NativeOptions::default();
+
     let _ = eframe::run_native(
-        "Rust drawer",
+        "FFT Viewer",
         options,
         Box::new(|cc| Ok(Box::new(app::RustyApp::new(cc)))),
     );
+}
+
+
+
+fn start_puffin_server() {
+    puffin::set_scopes_on(true); // tell puffin to collect data
+
+    match puffin_http::Server::new("127.0.0.1:8585") {
+        Ok(puffin_server) => {
+            println!("Run:  cargo install puffin_viewer && puffin_viewer --url 127.0.0.1:8585");
+
+            // std::process::Command::new("puffin_viewer")
+            //     .arg("--url")
+            //     .arg("127.0.0.1:8585")
+            //     .spawn()
+            //     .ok();
+
+            // We can store the server if we want, but in this case we just want
+            // it to keep running. Dropping it closes the server, so let's not drop it!
+            #[expect(clippy::mem_forget)]
+            std::mem::forget(puffin_server);
+        }
+        Err(err) => {
+            println!("Failed to start puffin server: {err}");
+        }
+    }
 }
